9bc6d42caffd8fec0dff04b05a2b87a4d3f54179
Hiroshi Akitaya <hiroshi.akitaya@gmail.com>
2023-03-31 02:41:02 +0900

    refpixtrim.py implemented. (= VIRGO reference pixel region removing tool)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kanatapy',
-    version='1.4.0',
+    version='1.4.1',
     description='Kanata Python tools',
     author='Hiroshi Akitaya',
     packages=find_packages(),
